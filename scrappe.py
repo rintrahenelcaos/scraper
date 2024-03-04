497ac05e75904f439efbccad28f14efe67b28808
Leonardo Mazzeo <leomazzeo@gmail.com>
2024-03-04 11:31:06 -0300

    struggle continues

--- scrappe.py
+++ scrappe.py
@@ -1,11 +1,18 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = "https://www.cohen.com.ar/Bursatil/Especie/BA.C"
+url = "https://en.wikipedia.org/wiki/List_of_countries_by_GDP_(PPP)"
 
 result = requests.get(url)
 
-doc = BeautifulSoup(result.text, "html.parser")
-    
-contenedor = doc.has_attr("id")
-print(contenedor)
+soup = BeautifulSoup(result.text, 'html.parser')
+
+for table in soup.find_all('table'):
+    print(table.get('class'))
+
+tables = soup.find_all('table')
+
+table = soup.find('table', class_='wikitable sortable sticky-header static-row-numbers')
+
+print(table)
+
